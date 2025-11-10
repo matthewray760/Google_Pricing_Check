6a9baaa2996a4d09b3a2e7917477e4fd86fc942c
matthewray760 <165307255+matthewray760@users.noreply.github.com>
2025-11-10 10:03:02 -0700

    Update sql.py

--- sql.py
+++ sql.py
@@ -157,4 +157,49 @@ def sql_cusip_id(account_id,identifiers,recon_date):
   
 
         return df, print("SQL executed within the timeout period")
+
+
+
+def sql_prom_pos(identifiers,recon_date,sleeve_agg):
+        # Set up the database connection parameters
+        server = 'P01-01-AG-004'
+        database = 'custodydata'
+        username = 'ARBFUND\matthewray'
+        password = 'Uhglbk547895207&'
+        driver = '{ODBC Driver 17 for SQL Server}'
+        
+
+        # Create the connection string
+        conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};Trusted_Connection=yes;TrustServerCertificate=yes;MultiSubnetFailover=yes'
+
+        # Connect to the database
+        conn = py.connect(conn_str)
+
+        # Create a cursor object to execute the SQL statements
+        cursor = conn.cursor()
+
+        cursor.execute('SET QUERY_GOVERNOR_COST_LIMIT 300')
+
+        # Execute a SQL query
+        query = f'''
+        SELECT DISTINCT pp.partitionID, pp.ReconDate, pp.Cusip [Custody Cusip], sm.Cusip [CW Cusip], sm.ID [Security ID]  from [data].[PrometheusPosition] pp
+        JOIN reportingLM.dbo.securitymaster sm on sm.ID = pp.SecurityID
+        WHERE partitionid IN ({sleeve_agg})
+        AND ReconDate = '{recon_date}'
+        AND sm.Cusip IN ({identifiers})
+        '''
+
+        cursor.execute(query)
+
+        # Fetch all the rows from the query result
+        cursor.fetchall()
+        df = pd.read_sql(query,conn)
+
+        # Close the cursor and the connection
+        cursor.close()
+        conn.close()
+  
+
+        return df, print("SQL executed within the timeout period")
+
     
